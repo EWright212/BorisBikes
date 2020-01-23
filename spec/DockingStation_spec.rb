f0a1eadcd9cdc6fa608727525137a8e46d904c1c
Student <student@Makerss-MacBook-Air.local>
2020-01-23 15:30:08 +0000

    Chapter 12 complete

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -16,10 +16,15 @@ describe DockingStation do
       expect(subject.bike).to eq bike
     end
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
+  describe 'releases bikes' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+     end
 
+     it 'raises an error when there are no bike available' do 
+      expect {subject.release_bike}.to raise_error('No bikes available')
+    end
+   end
 end
-
