bc48941f84e4a9bfb77253adc236a93cf2037d38
Student <student@Makerss-MacBook-Air.local>
2020-01-23 16:47:00 +0000

    chapter 14

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,16 +1,20 @@
 require_relative 'Bike'
 
 class DockingStation
-    attr_reader :bike
+    
+    def initialize
+    @bike_storage = []
+    end
     
     def release_bike
-      fail 'No bikes available' unless @bike
-      @bike
+      fail 'No bikes available' if @bike_storage.empty?
+      @bike_storage.pop
     end
 
    def dock(bike)
-      fail 'Cannot add another bike: Capacity full' if @bike
-      @bike = bike
+      fail 'Cannot add another bike: Capacity full' if @bike_storage.count >= 20
+      @bike_storage << bike
+      return @bike_storage[0]
    end
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,7 +3,7 @@ require 'DockingStation'
 describe DockingStation do
     it { is_expected.to respond_to(:release_bike) }
     it { is_expected.to respond_to(:dock).with(1).argument }
-    it { is_expected.to respond_to(:bike)}
+    # it { is_expected.to respond_to(:bike)} 
     
     it '#docks something' do
       bike = Bike.new
@@ -13,13 +13,13 @@ describe DockingStation do
     it '#returns docked bikes' do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.bike).to eq bike
+      expect(subject.dock(bike)).to eq bike
     end
   
   describe '#dock' do
-    it '#raises an error when an extra bike is added at full capacity' do 
-      subject.dock(Bike.new)
-      expect {subject.dock(Bike.new)}.to raise_error('Cannot add another bike: Capacity full')
+    it '#raises an error when full' do 
+      20.times {subject.dock Bike.new}
+      expect {subject.dock Bike.new}.to raise_error('Cannot add another bike: Capacity full')
     end
   end
 
